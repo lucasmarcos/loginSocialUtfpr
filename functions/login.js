f688b3342570a59e696e07484b8a24ccbb24ea16
Lucas Marcos <lucasmarcos@live.com>
2024-04-15 18:02:36 -0300

    functions

--- functions/login.js
+++ functions/login.js
@@ -1,3 +1,20 @@
-export function onRequest(context) {
-	return new Response("login");
+export async function onRequestPost(context) {
+	const json = await context.request.json();
+
+	const { username, password } = json;
+
+	const request = new Request(
+		"https://sistemas2.utfpr.edu.br/utfpr-auth/api/v1",
+		{
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+				Referer: "http://sistemas2.utfpr.edu.br",
+			},
+			body: JSON.stringify({ username, password }),
+		},
+	);
+
+	const response = await fetch(request);
+	return response;
 }
