4af4873872df7be89f6bd332edc525f4988cb95b
Lucas Marcos <lucasmarcos@live.com>
2024-04-15 18:05:07 -0300

    noaxios

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,3 @@
-import axios from "axios";
 import { decodeJwt } from "jose";
 import { useState } from "react";
 import { createRoot } from "react-dom/client";
@@ -9,32 +8,13 @@ const App = () => {
 	const [token, setToken] = useState();
 
 	const submit = async () => {
-		//		const a = await axios({
-		//			method: "POST",
-		//			url: "https://sistemas2.utfpr.edu.br/utfpr-auth/api/v1",
-		//			data: { username, password },
-		//			headers: {
-		//				Referer: "http://utfpr.edu.br",
-		//				"Referrer-Policy": "unsafe-url",
-		//			},
-		//		});
-		//
-		//		console.log(a);
-		//		setToken(a.data.token);
-
-		const request = new Request(
-			"https://sistemas2.utfpr.edu.br/utfpr-auth/api/v1",
-			{
-				method: "POST",
-				headers: {
-					"Content-Type": "application/json",
-				},
-				referrer: "http://sistemas2.utfpr.edu.br",
-				referrerPolicy: "unsafe-url",
-				body: `{ "username": "${username}", "password": "${password}" }`,
+		const request = new Request("/login", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
 			},
-		);
-		console.log(request);
+			body: `{ "username": "${username}", "password": "${password}" }`,
+		});
 		const response = await fetch(request);
 		const json = await response.json();
 		setToken(json.token);
