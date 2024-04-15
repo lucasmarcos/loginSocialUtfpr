63e672de91976ca94adfe1dd46fa471eada25872
Lucas Marcos <lucasmarcos@live.com>
2024-04-15 15:46:40 -0300

    init

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import { createRoot } from "react-dom/client";
+import { useState } from "react";
+
+const App = () => {
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+const root = createRoot(document.getElementById("root"));
+
+root.render(<App/>);
